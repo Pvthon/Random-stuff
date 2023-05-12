7a7fd3d25b8eeae2aaaccf3c159de7d5c6009fbd
James <133438816+Snitrovirx@users.noreply.github.com>
2023-05-12 22:58:29 +0100

    Add files via upload

---
+++ Simple_chatbot.py
@@ -0,0 +1,27 @@
+import time
+name = input("hello my name is james what is your name?: ")
+time.sleep(2)
+print("Hello "+name)
+
+time.sleep(2)
+
+hight = input("are you tall?: ")
+time.sleep(2)
+if hight == ("yes"): print ("Good for you!")
+if hight == ("no"): print ("Cool!")
+
+time.sleep(2)
+
+how = input
+print("Anyway, how are you " +name)
+time.sleep(2)
+if how == ("fine"): print ("Great!")
+if how == ("im fine"): print ("Great!")
+if how == ("im good"): print ("Great!")
+if how == ("good"): print ("Great!")
+if how == ("bad"): print ("Oh no!")
+if how == ("im bad"): print ("Oh no!")
+if how == ("im sad "): print ("Oh no!")
+if how == ("im ok"): print ("Alright")
+
+time.sleep(2)
